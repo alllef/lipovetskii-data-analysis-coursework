27a41393539879a95bbc9188940ca2ead85d1fe4
alllef <alekz29@gmail.com>
2021-05-12 18:14:45 +0300

    correct game page parsing

--- src/main/java/com/github/lipovetskii/data_parsing/GamePage.java
+++ src/main/java/com/github/lipovetskii/data_parsing/GamePage.java
@@ -6,7 +6,9 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GamePage {
@@ -22,12 +24,12 @@ public class GamePage {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Map<String, String> getTimeToBeat = getTimeToBeat();
+        Map<String,Integer> getTimeToBeat = getTimeToBeat();
         Map<String, String> getAddiInformation = getAdditionalInformation();
 
     }
 
-    public Map<String, String> getTimeToBeat() {
+    public Map<String, Integer> getTimeToBeat() {
         Map<String, String> timesToBeat = new HashMap<>();
         Elements times = doc.select(".game_times>ul>li");
 
@@ -37,22 +39,38 @@ public class GamePage {
             timesToBeat.put(key, value);
         }
 
-        return timesToBeat;
+        return filterTimeToBeat(timesToBeat);
+    }
+
+
+    private Map<String, Integer> filterTimeToBeat(Map<String, String> timeToBeat) {
+        Map<String, Integer> filteredTimeToBeat = new HashMap<>();
+
+        for (Map.Entry<String, String> pair : timeToBeat.entrySet()) {
+            double minutes = Double.parseDouble(pair.getValue().split(" ")[0].replace("½", "0.5"));
+            if (pair.getValue().contains("Hours")) minutes *= 60;
+            filteredTimeToBeat.put(pair.getKey(), (int) minutes);
+        }
+
+        return filteredTimeToBeat;
     }
 
     public Map<String, String> getAdditionalInformation() {
 
+        List<String> pluralFormKeys = List.of("Publishers", "Developers", "Genres", "Platforms");
         Map<String, String> additionalInformation = new HashMap<>();
         Elements addInfoElements = doc.select("[class=\"profile_info\"],[class=\"profile_info medium\"]");
 
         for (Element elem : addInfoElements) {
-            String key = elem.select("strong").text();
-            String value = elem.text();
-            additionalInformation.put(key, value);
+            String key = elem.select("strong").text().replace(":", "");
+            String value = elem.text().replace(key + ": ", "");
+            if (pluralFormKeys.contains(key)) additionalInformation.put(key.substring(0, key.length() - 1), value);
+            else additionalInformation.put(key,value);
         }
 
         return additionalInformation;
 
     }
+
 
 }
